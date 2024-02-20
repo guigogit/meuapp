c84f32a52de03b96d9d4d32640b7553b2dc1c96e
rodrigo <rodrigo.oliveira@spyonsky.com.br>
2024-02-20 15:01:52 -0300

    Aula_204

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component'; // Importe o AppComponent
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
   declarations: [
+
     // AppComponent já é declarado no AppModule, não é necessário declará-lo novamente aqui
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+
+
   ],
   providers: [],
   //bootstrap: [AppComponent]
