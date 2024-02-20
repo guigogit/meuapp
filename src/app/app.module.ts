7e4a781439de73907fa69ea778f96c6fc1c8e0a3
rodrigo <rodrigo.oliveira@spyonsky.com.br>
2024-02-20 11:40:35 -0300

    Projeto_angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
+// app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component'; // Importe o AppComponent
+
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    // AppComponent já é declarado no AppModule, não é necessário declará-lo novamente aqui
+  ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: []
+  //bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
+import { AppModule } from './app/app.module'; // Importe o módulo AppModule
 import { environment } from './environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
+// Use a função bootstrapApplication para inicializar o aplicativo
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
