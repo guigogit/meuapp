58c3efc2748f028bb22e2d93cdb4b7a416ecefcf
rodrigo <rodrigo.oliveira@spyonsky.com.br>
2024-02-21 16:31:19 -0300

    Aula_206

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,59 @@
-// app.module.ts
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component'; // Importe o AppComponent
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatBadgeModule} from '@angular/material/badge';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+
+
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 
 @NgModule({
   declarations: [
 
-    // AppComponent já é declarado no AppModule, não é necessário declará-lo novamente aqui
   ],
   imports: [
+    MatToolbarModule,
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    HttpClientModule,
+    MatSlideToggleModule,
     MatButtonModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    MatCardModule,
+    MatDividerModule,
+    MatDialogModule,
+    MatChipsModule,
+    MatBadgeModule,
 
 
   ],
-  providers: [],
-  //bootstrap: [AppComponent]
+  providers: [
+
+
+    provideAnimationsAsync()
+  ],
+
+
 })
 export class AppModule { }
+
