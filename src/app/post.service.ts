5466f5165a3072d686994dcf920a0e61fc1c1bdd
rodrigo <rodrigo.oliveira@spyonsky.com.br>
2024-07-24 15:31:01 -0300

    Aula_220_meuapp

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,16 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Post } from './post';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class PostService {
-  public posts: Post[] = [
-    new Post("João", "Meu post", "Sub Meu", "joao@gmail.com", "Minha MSG"),
-    new Post("Paulo", "Post do Paulo", "Sub Paulo", "paulo@gmail.com", "MSG do Paulo"),
-    new Post("Maria", "Post da Maria", "Sub Maria", "Maria@gmail.com", "MSG da Maria"),
-    new Post("Rodrigo", "Post do Rodrigo", "Sub Rodrigo", "Rodrigo@gmail.com", "MSG do Rodrigo"),
-    new Post("Mirly", "Post da Mirly", "Sub Mirly", "Mirly@gmail.com", "MSG da Mirly"),
-  ];
+  public posts: Post[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.fetchPosts().subscribe(
+      (posts: Post[]) => {
+        for (let p of posts) {
+          this.posts.push(
+            new Post(p.nome, p.titulo, p.subtitulo, p.email, p.mensagem, p.arquivo, p.id, p.likes)
+          );
+        }
+      }
+    );
+  }
+
+  fetchPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>('/api');
+  }
 }
