8d9a9b01f118721b444ebe38b0afb45000c6886d
rodrigo <rodrigo.oliveira@spyonsky.com.br>
2024-07-24 16:36:18 -0300

    Aula_220_meuapp

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpEvent, HttpEventType } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Post } from './post';
 import { Observable } from 'rxjs';
@@ -21,5 +21,27 @@ export class PostService {
 
   fetchPosts(): Observable<Post[]> {
     return this.http.get<Post[]>('/api');
+  }
+
+  salvar(post: Post, file: File){
+    const uploadData = new FormData();
+    uploadData.append('nome', post.nome);
+    uploadData.append('email', post.email);
+    uploadData.append('titulo', post.titulo);
+    uploadData.append('subtitulo', post.subtitulo);
+    uploadData.append('mensagem', post.mensagem);
+    uploadData.append('arquivo', file, file.name);
+
+    this.http.post("/api", uploadData, {
+      reportProgress: true,
+      observe: 'events'
+    }).subscribe((event: HttpEvent<any>) => {
+      if (event.type === HttpEventType.UploadProgress) {
+        const percentDone = Math.round(100 * event.loaded / event.total!);
+        console.log(`File is ${percentDone}% uploaded.`);
+      } else if (event.type === HttpEventType.Response) {
+        console.log('File uploaded successfully!', event.body);
+      }
+    });
   }
 }
